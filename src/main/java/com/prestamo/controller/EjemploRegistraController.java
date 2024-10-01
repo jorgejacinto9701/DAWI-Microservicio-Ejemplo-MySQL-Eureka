f4d9222338e15baa4a39a775d863d80105e78e0a
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-10-01 16:19:35 -0500

    ajustes

--- src/main/java/com/prestamo/controller/EjemploRegistraController.java
+++ src/main/java/com/prestamo/controller/EjemploRegistraController.java
@@ -3,15 +3,20 @@ package com.prestamo.controller;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prestamo.entity.Ejemplo;
@@ -58,6 +63,77 @@ public class EjemploRegistraController {
 			 return "{\"valid\":false}";
 		 }
 			
+	}
+	
+	@GetMapping("/listaEjemploPorNombreLike/{var}")
+	@ResponseBody
+	public ResponseEntity<?> listaEjemploPorNombreLike(@PathVariable("var") String nombre){
+		List<Ejemplo> lstSalida = null;
+		if (nombre.equals("todos")) {
+			lstSalida =ejemploService.listaEjemplo();
+		}else {
+			lstSalida =ejemploService.listaEjemploPorDescripcionLike(nombre +  "%");
+		}
+		return ResponseEntity.ok(lstSalida);
+	}
+	
+	@PostMapping("/registraEjemplo")
+	@ResponseBody
+	public ResponseEntity<?> insertaEjemplo(@RequestBody Ejemplo obj) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			obj.setIdEjemplo(0);
+			obj.setFechaActualizacion(new Date());
+			obj.setFechaRegistro(new Date());
+			obj.setEstado(AppSettings.ACTIVO);
+			
+			Ejemplo objSalida = ejemploService.insertaActualizaEjemplo(obj);
+			if (objSalida == null) {
+				salida.put("mensaje", AppSettings.MENSAJE_REG_ERROR);
+			} else {
+				salida.put("mensaje", AppSettings.MENSAJE_REG_EXITOSO + " Ejemplo de ID ==> " + obj.getIdEjemplo() + ".");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", AppSettings.MENSAJE_REG_ERROR);
+		}
+		return ResponseEntity.ok(salida);
+	}
+
+	@PutMapping("/actualizaEjemplo")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> actualizaEjemplo(@RequestBody Ejemplo obj) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			
+			obj.setFechaActualizacion(new Date());
+
+			Ejemplo objSalida = ejemploService.insertaActualizaEjemplo(obj);
+			if (objSalida == null) {
+				salida.put("mensaje", AppSettings.MENSAJE_ACT_ERROR);
+			} else {
+				salida.put("mensaje", AppSettings.MENSAJE_ACT_EXITOSO + " Ejemplo de ID ==> " + obj.getIdEjemplo() + ".");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", AppSettings.MENSAJE_ACT_ERROR);
+		}
+		return ResponseEntity.ok(salida);
+	}
+	
+	
+	@DeleteMapping("/eliminaEjemplo/{id}")
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> eliminaEjemplo(@PathVariable("id") int idEjemplo) {
+		Map<String, Object> salida = new HashMap<>();
+		try {
+			ejemploService.eliminaEjemplo(idEjemplo);
+			salida.put("mensaje", AppSettings.MENSAJE_ELI_EXITOSO + " Ejemplo de ID ==> " + idEjemplo + "." );
+		} catch (Exception e) {
+			e.printStackTrace();
+			salida.put("mensaje", AppSettings.MENSAJE_ELI_ERROR);
+		}
+		return ResponseEntity.ok(salida);
 	}
 }
 
